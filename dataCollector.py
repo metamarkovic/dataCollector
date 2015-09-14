dd5dddc67518b0c345dbc6fbb5a13bc9113d47f5
Marjeta Markovic <markovic.marjeta@gmail.com>
2015-09-14 12:01:44 +0200

    add NOVIRUS condition data collection
    	diseaseData2.csv

--- dataCollector.py
+++ dataCollector.py
@@ -2,17 +2,19 @@ __author__ = 'meta'
 
 import os
 from getDistanceForTraceFile import TraceDistance
+from distanceCalc import DistanceCalc
 import csv
 import glob
 import xml.etree.cElementTree as ET
 
 
 class DataCollector:
-    fieldnames = ['Ind_ID', 'Exp_Num', 'probability','euclideanStep', 'manhattanStep', 'euclideanTotal','manhattanTotal', 'lifetime','size', 'totalMuscles','totalFat','totalBone','originalMuscle','originalFat','originalBone']
+    fieldnames = ['Ind_ID', 'birthtime', 'Exp_Num', 'probability','euclideanStep', 'manhattanStep', 'euclideanTotal','manhattanTotal', 'lifetime','size', 'totalMuscles','totalFat','totalBone','originalMuscle','originalFat','originalBone','originalEuclideanStep', 'originalManhattanStep', 'originalEuclideanTotal','originalManhattanTotal']
 
     def dataCollector(self):
-        listing = glob.glob('../EC14-Exp-10*')
+        listing = glob.glob('../EC14-Exp-1*')
         td = TraceDistance()
+        dc = DistanceCalc()
         with open('diseaseData.csv', 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
             writer.writeheader()
@@ -30,15 +32,27 @@ class DataCollector:
                     print "Backups path: " + backup_path # Testing
                     voxProbability, voxLifetime, voxCounts = self.voxCounter(experimentNumber,path, indNumber)
                     voxProb_orig, voxProbLife_orig, voxCounts_orig = self.voxCounter(experimentNumber,backup_path, indNumber)
+
                     traceFilename = os.path.abspath(nestFile + "/traces_afterPP/" + indNumber + ".trace")
+                    NOMUT_traceFilename = os.path.abspath(nestFile + "/traces_NOVIRUS/" + indNumber + ".trace")
                     try:
                         distances = td.calcDistance(traceFilename)
-                        # print distances # Testing
+                        birthtime = dc.getBirthTime(traceFilename)
+                        print distances # Testing
+                        print 'Born at: ' + str(birthtime)
                     except IOError:
                         distances = ['NA', 'NA', 'NA', 'NA', 'NA']
+                        birthtime = ['NA']
                         print indNumber +  " trace file missing in /traces_afterPP/ of experiment " + experimentNumber
 
-                    writer.writerow({'Ind_ID': indNumber, 'Exp_Num': experimentNumber, 'probability': voxProbability, 'euclideanStep': distances[0], 'manhattanStep': distances[1], 'euclideanTotal': distances[2], 'manhattanTotal': distances[3], 'lifetime': voxLifetime, 'size': voxCounts[4], 'totalMuscles': voxCounts[3], 'totalFat': voxCounts[1], 'totalBone': voxCounts[2],'originalMuscle': voxCounts_orig[3],'originalFat': voxCounts_orig[1],'originalBone': voxCounts_orig[2]})
+                    try:
+                        NOMUT_distacnes = td.calcDistance(NOMUT_traceFilename)
+                        print NOMUT_distacnes
+                    except IOError:
+                        NOMUT_distacnes = ['NA', 'NA', 'NA', 'NA', 'NA']
+                        print indNumber +  " trace file missing in /traces_NOMUT/ of experiment " + experimentNumber
+
+                    writer.writerow({'Ind_ID': indNumber,'birthtime': birthtime, 'Exp_Num': experimentNumber, 'probability': voxProbability, 'euclideanStep': distances[0], 'manhattanStep': distances[1], 'euclideanTotal': distances[2], 'manhattanTotal': distances[3], 'lifetime': voxLifetime, 'size': voxCounts[4], 'totalMuscles': voxCounts[3], 'totalFat': voxCounts[1], 'totalBone': voxCounts[2],'originalMuscle': voxCounts_orig[3],'originalFat': voxCounts_orig[1],'originalBone': voxCounts_orig[2],'originalEuclideanStep':NOMUT_distacnes[0], 'originalManhattanStep':NOMUT_distacnes[1], 'originalEuclideanTotal':NOMUT_distacnes[2],'originalManhattanTotal':NOMUT_distacnes[3]})
                     # except KeyboardInterrupt:
                     #     quit()
 
