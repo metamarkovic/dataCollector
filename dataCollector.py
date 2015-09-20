900e7b9bbdf02d2015e37209d12d58d98891b515
fgolemo <fgolemo@gmail.com>
2015-09-21 01:08:47 +0200

    reformatted datacollector and added command line argument

--- dataCollector.py
+++ dataCollector.py
@@ -1,5 +1,3 @@
-__author__ = 'meta'
-
 import os
 from getDistanceForTraceFile import TraceDistance
 from distanceCalc import DistanceCalc
@@ -7,9 +5,20 @@ import csv
 import glob
 import xml.etree.cElementTree as ET
 
+__author__ = 'meta'
+
 
 class DataCollector:
-    fieldnames = ['Ind_ID', 'birthtime', 'Exp_Num', 'probability','euclideanStep', 'manhattanStep', 'euclideanTotal','manhattanTotal', 'lifetime','size', 'totalMuscles','totalFat','totalBone','originalMuscle','originalFat','originalBone','originalEuclideanStep', 'originalManhattanStep', 'originalEuclideanTotal','originalManhattanTotal']
+    fieldnames = ['Ind_ID', 'birthtime', 'Exp_Num', 'probability', 'euclideanStep', 'manhattanStep', 'euclideanTotal',
+                  'manhattanTotal', 'lifetime', 'size', 'totalMuscles', 'totalFat', 'totalBone', 'originalMuscle',
+                  'originalFat', 'originalBone', 'originalEuclideanStep', 'originalManhattanStep',
+                  'originalEuclideanTotal', 'originalManhattanTotal']
+
+    def __init__(self, pattern):
+        if not pattern:
+            self.pattern = '../EC14-Exp-1*'
+        else:
+            self.pattern = pattern
 
     def dataCollector(self):
         listing = glob.glob('../EC14-Exp-1*')
@@ -18,44 +27,53 @@ class DataCollector:
         with open('diseaseData.csv', 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
             writer.writeheader()
-            print  listing # Testing
+            print  listing  # Testing
             for nestFile in listing:
-                print "Experiment folder: " + nestFile # Testing
+                print "Experiment folder: " + nestFile  # Testing
                 expFolder = nestFile[-2:]
                 experimentNumber = str(expFolder)
                 popListing = glob.glob(nestFile + "/population/*.vxa")
                 for path in popListing:
                     indNumber = path[27:-8]
-                    print "Population path: " + path # Testing
-                    print "Ind. ID: " + indNumber # Testing
+                    print "Population path: " + path  # Testing
+                    print "Ind. ID: " + indNumber  # Testing
                     backup_path = os.path.abspath(nestFile + "/population_beforePL/" + indNumber + "_vox.vxa")
-                    print "Backups path: " + backup_path # Testing
-                    voxProbability, voxLifetime, voxCounts = self.voxCounter(experimentNumber,path, indNumber)
-                    voxProb_orig, voxProbLife_orig, voxCounts_orig = self.voxCounter(experimentNumber,backup_path, indNumber)
+                    print "Backups path: " + backup_path  # Testing
+                    voxProbability, voxLifetime, voxCounts = self.voxCounter(experimentNumber, path, indNumber)
+                    voxProb_orig, voxProbLife_orig, voxCounts_orig = self.voxCounter(experimentNumber, backup_path,
+                                                                                     indNumber)
 
                     traceFilename = os.path.abspath(nestFile + "/traces_afterPP/" + indNumber + ".trace")
                     NOMUT_traceFilename = os.path.abspath(nestFile + "/traces_NOVIRUS/" + indNumber + ".trace")
                     try:
                         distances = td.calcDistance(traceFilename)
                         birthtime = dc.getBirthTime(traceFilename)
-                        print distances # Testing
+                        print distances  # Testing
                         print 'Born at: ' + str(birthtime)
                     except IOError:
                         distances = ['NA', 'NA', 'NA', 'NA', 'NA']
                         birthtime = ['NA']
-                        print indNumber +  " trace file missing in /traces_afterPP/ of experiment " + experimentNumber
+                        print indNumber + " trace file missing in /traces_afterPP/ of experiment " + experimentNumber
 
                     try:
                         NOMUT_distacnes = td.calcDistance(NOMUT_traceFilename)
                         print NOMUT_distacnes
                     except IOError:
                         NOMUT_distacnes = ['NA', 'NA', 'NA', 'NA', 'NA']
-                        print indNumber +  " trace file missing in /traces_NOMUT/ of experiment " + experimentNumber
+                        print indNumber + " trace file missing in /traces_NOMUT/ of experiment " + experimentNumber
 
-                    writer.writerow({'Ind_ID': indNumber,'birthtime': birthtime, 'Exp_Num': experimentNumber, 'probability': voxProbability, 'euclideanStep': distances[0], 'manhattanStep': distances[1], 'euclideanTotal': distances[2], 'manhattanTotal': distances[3], 'lifetime': voxLifetime, 'size': voxCounts[4], 'totalMuscles': voxCounts[3], 'totalFat': voxCounts[1], 'totalBone': voxCounts[2],'originalMuscle': voxCounts_orig[3],'originalFat': voxCounts_orig[1],'originalBone': voxCounts_orig[2],'originalEuclideanStep':NOMUT_distacnes[0], 'originalManhattanStep':NOMUT_distacnes[1], 'originalEuclideanTotal':NOMUT_distacnes[2],'originalManhattanTotal':NOMUT_distacnes[3]})
+                    writer.writerow({'Ind_ID': indNumber, 'birthtime': birthtime, 'Exp_Num': experimentNumber,
+                                     'probability': voxProbability, 'euclideanStep': distances[0],
+                                     'manhattanStep': distances[1], 'euclideanTotal': distances[2],
+                                     'manhattanTotal': distances[3], 'lifetime': voxLifetime, 'size': voxCounts[4],
+                                     'totalMuscles': voxCounts[3], 'totalFat': voxCounts[1], 'totalBone': voxCounts[2],
+                                     'originalMuscle': voxCounts_orig[3], 'originalFat': voxCounts_orig[1],
+                                     'originalBone': voxCounts_orig[2], 'originalEuclideanStep': NOMUT_distacnes[0],
+                                     'originalManhattanStep': NOMUT_distacnes[1],
+                                     'originalEuclideanTotal': NOMUT_distacnes[2],
+                                     'originalManhattanTotal': NOMUT_distacnes[3]})
                     # except KeyboardInterrupt:
                     #     quit()
-
 
     def voxCounter(self, expNum, filepath, ID):
         try:
@@ -67,20 +85,29 @@ class DataCollector:
             for layer in layers:
                 dna += str(layer.text).strip()
             dna_length = len(dna)
-            count = [dna.count('0'),dna.count('1'),dna.count('2'),(dna.count('3') + dna.count('4')),(dna.count('1') + dna.count('2') + dna.count('3') + dna.count('4'))]
-            print count # Testing
-            print "SUM: " + str(sum(count)-count[4]) # Testing
+            count = [dna.count('0'), dna.count('1'), dna.count('2'), (dna.count('3') + dna.count('4')),
+                     (dna.count('1') + dna.count('2') + dna.count('3') + dna.count('4'))]
+            print count  # Testing
+            print "SUM: " + str(sum(count) - count[4])  # Testing
             probability = (count[1] / 1000.0) * 0.5
-            print "Probability: " + str(probability) # Testing
-            if dna_length != (sum(count)-count[4]):
-                count = ["NA","NA","NA","NA","NA"]
+            print "Probability: " + str(probability)  # Testing
+            if dna_length != (sum(count) - count[4]):
+                count = ["NA", "NA", "NA", "NA", "NA"]
                 print "DNA length count error for " + ID + " in /population/ of experiment " + expNum
         except ET.ParseError:
-            count = ["NA","NA","NA","NA","NA"]
+            count = ["NA", "NA", "NA", "NA", "NA"]
             probability = "NA"
             lifetime = "NA"
             print ID + "_vox.vxa file missing in " + filepath + " of experiment " + expNum
         return (probability, lifetime, count)
 
-dataCol = DataCollector()
-dataCol.dataCollector()
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 2:
+        pattern = sys.argv[1]
+    else:
+        pattern = False
+    dataCol = DataCollector(pattern)
+    dataCol.dataCollector()
