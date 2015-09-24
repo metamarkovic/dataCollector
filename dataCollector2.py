d57be563bf59ed304233c2e5cec062fa25e310e9
Florian Golemo <fgolemo@gmail.com>
2015-09-24 11:08:41 +0200

    added type detection

--- dataCollector2.py
+++ dataCollector2.py
@@ -22,7 +22,18 @@ class DataCollector2:
             self.outputFile = 'data.csv'
         else:
             self.outputFile = outputFile
+
         self.expNumberRegex = re.compile('([0-9]+)$')
+        self.traceFolderNormal = "traces_afterPP"
+        self.traceFoldersAlt = {
+            "with disease": "traces_NOMUT",
+            "no disease": "traces_MUT"
+        }
+        self.populationFolderNormal = "population"
+        self.populationFoldersAlt = {
+            "with disease": "population_beforePL",
+            "no disease": "population_MUT"
+        }
 
     def getExperiments(self):
         expFolders = glob.glob(self.pattern)
@@ -42,18 +53,55 @@ class DataCollector2:
 
     def collectData(self):
         experiments = self.getExperiments()
-        print experiments
-        quit()
         for exp in experiments:
             type = self.getType(exp)
+            # print exp[0],type
             individuals = self.getIndividuals(exp)
             for indiv in individuals:
                 features = self.getFeatures(exp, type, indiv)
                 self.writeFeatures(exp, type, indiv, features)
 
-    def writeOutput(self):
-        pass
+    def getType(self, experiment):
+        # if the alternative traces DON'T a disease then the main experiment DID have a disease
+        if self.hasAltPopWithoutDisease(experiment):
+            return "with disease"
+        # if the alternative traces DO have a disease then the main experiment DIDN'T have a disease
+        if self.hasAltPopWithDisease(experiment):
+            if not self.hasAltPopWithoutDisease(experiment):
+                return "no disease"
+            else:
+                self.errorHasBothPopFiles(experiment)
+        # if neither is the case, then there are no trace files for this experiment... abort
+        self.errorHasNoPop()
 
+    def hasAltPopWithoutDisease(self, experiment):
+        return self.hasAltPop(experiment, "no disease")
+
+    def hasAltPopWithDisease(self, experiment):
+        return self.hasAltPop(experiment, "with disease")
+
+    def hasAltPop(self, experiment, condition):
+        altPopPath = experiment[2] + os.path.sep + self.populationFoldersAlt[condition]
+        if not os.path.isdir(altPopPath):
+            return False
+        if len(os.listdir(altPopPath)) > 0:
+            return True
+        return False
+
+    @staticmethod
+    def errorHasBothPopFiles(experiment):
+        print "ERROR: this shouldn't happen - an experiment has alternative population files " \
+                      "both WITH and WITHOUT disease in addition to the normal experiment traces:"
+        print experiment
+        print "...Please fix this before continuing. Exiting."
+        quit()
+
+    @staticmethod
+    def errorHasNoPop(experiment):
+        print "ERROR: the following experiment has no alternative population files (neither with disease nor without):"
+        print experiment
+        print "...Please fix this before continuing. Exiting."
+        quit()
 
 if __name__ == "__main__":
     import sys
