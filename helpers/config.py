1db7434e4e3694a58b405f9979dc40adb2ed738b
Florian Golemo <fgolemo@gmail.com>
2015-09-28 15:33:22 +0200

    added some comment for clarification in config file

--- helpers/config.py
+++ helpers/config.py
@@ -1,5 +1,5 @@
 class PathConfig:
-    types = ["no disease", "with disease"] # index 0 = no dis, index 1 = with dis
+    types = ["no disease", "with disease"]  # index 0 = no dis, index 1 = with dis
     traceFolderNormal = "traces_afterPP"
     traceFoldersAlt = {
         "with disease": "traces_NOMUT",
@@ -7,7 +7,9 @@ class PathConfig:
     }
     populationFolderNormal = "population"
     populationFoldersAlt = {
+        # if the original population has the disease ("with disease"), then there is a pop folder without disease ("..._beforePL")
         "with disease": "population_beforePL",
+        # if the original population has no disease then there is a folder with additional mutation
         "no disease": "population_MUT"
     }
 
