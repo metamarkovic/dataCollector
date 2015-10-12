51d4e250ca8105430bb881338d4e6174c7e0d86b
Luka Stout <lukastout@gmail.com>
2015-10-12 18:23:38 +0200

    Fix for faulty trace files

--- featureExtractors/Gait.py
+++ featureExtractors/Gait.py
@@ -7,7 +7,19 @@ class Gait(FeatureExtractorAbstract):
 
     def getCSVheader(self):
         return ["gaitPeriod"]
+ 
 
+    # isValidLine and sameAsFloat have been copied from distanceCalc.py
+    def isValidLine(self, lineSplit):
+        return len(lineSplit) == 5 and self.sameAsFloat(lineSplit[2]) and self.sameAsFloat(lineSplit[3])
+
+    @staticmethod
+    def sameAsFloat(input):
+        try:
+            floatInput = float(input)
+            return str(floatInput) == str(input)
+        except ValueError:
+            return False
 
     def extract(self, experiment, type, indiv):
         filepath = experiment[2] + os.path.sep + PathConfig.traceFolderNormal + os.path.sep + indiv[0] + ".trace"
@@ -20,9 +32,13 @@ class Gait(FeatureExtractorAbstract):
         with open(filepath) as fh:
             zs = []
             for line in fh:
-                line = line.strip().split()
+                lineSplit = line.split("\t")
+                if not self.isValidLine(lineSplit):
+                    lineSplit = line.split(" ")
+                    if not self.isValidLine(lineSplit):
+                        continue
 
-                zs.append(line[-1])
+                zs.append(lineSplit[-1])
             
         z_fft = np.fft.rfft(zs).real
         z_fft = z_fft[range(len(z_fft)/2+1)]
